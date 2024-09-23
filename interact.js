444da5ffe3961932f348ad81a3066b3e175d3752
Emilyempa <emilypettersson@hotmail.com>
2024-09-23 17:01:44 +0200

    first commit

---
+++ interact.js
@@ -0,0 +1,84 @@
+
+const tasks = []
+let completedCount = 0;
+
+
+
+document.querySelector('#submit').addEventListener('click', function() {
+    
+
+    //testing adding icons i js instead of html
+    const checkIcon = document.createElement("img");
+    checkIcon.src = "./pics/check.svg";
+    checkIcon.alt = "Check"
+    checkIcon.className = "check-icon"
+
+    const deleteIcon = document.createElement("img");
+    deleteIcon.src = "./pics/delete.svg"; 
+    deleteIcon.alt = "Delete";
+    deleteIcon.className = "delete-icon";
+
+    const taskInput = document.querySelector('#input');
+    const taskText = taskInput.value;
+
+    const deleteAll = document.querySelector("#deleteAll");
+        
+    //checking if string is empty and if so adding an alert
+    if (taskText.length == "") {
+        alert("Ooops you forgot to add a task!")
+        return
+    }
+        //creating a list
+        const li = document.createElement('li');
+        li.textContent = taskText;
+            
+        // adding listner to delete-icon
+        deleteIcon.addEventListener('click', function() {
+            li.remove();
+        });
+
+    
+        // adding icons to li-elementet
+        li.appendChild(checkIcon);
+        li.appendChild(deleteIcon);
+
+        // adding li-elementet to list
+        document.querySelector("#tasks").appendChild(li);
+
+        // clearing input and adding placeholder
+        taskInput.value = '';
+        
+        
+        // delete all tasks
+        deleteAll.addEventListener("click", function() {
+            document.querySelector('#tasks').innerHTML = '';
+        });
+        
+        //Working to make task complete in HTML but not counting correct
+        checkIcon.addEventListener("click", function() {  
+            li.classList.toggle("completed");
+            if (li.classList.contains("completed")) {
+                completedCount++;
+            } else {
+                
+                completedCount--;
+            }
+
+        
+        
+        document.querySelector("#completedCount").innerHTML = "Completed tasks: " + completedCount;       
+         
+         });
+         
+
+       
+
+
+    // add to array
+    tasks.push({ text: taskText, completed: false });
+});
+
+
+
+
+
