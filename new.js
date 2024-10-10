cb7481689f7cbe9147b8821b0c0feb850df91d43
Emilyempa <emilypettersson@hotmail.com>
2024-10-10 14:58:30 +0200

    fixing format

--- new.js
+++ new.js
@@ -5,81 +5,69 @@ const tasks = document.querySelector("#tasks");
 const taskInput = document.querySelector("#input");
 // const deleteAll = document.querySelector("#deleteAll");
 
-document.querySelector('#submit').addEventListener('click', function() {
-    
+document.querySelector("#submit").addEventListener("click", function () {
+  //testing adding icons i js instead of html
+  // const checkIcon = document.createElement("img");
+  // checkIcon.src = "./pics/check.svg";
+  // checkIcon.alt = "Check";
+  // checkIcon.className = "check-icon";
 
-    //testing adding icons i js instead of html
-    // const checkIcon = document.createElement("img");
-    // checkIcon.src = "./pics/check.svg";
-    // checkIcon.alt = "Check";
-    // checkIcon.className = "check-icon";
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "./pics/delete.svg";
+  deleteIcon.alt = "Delete";
+  deleteIcon.className = "delete-icon";
 
-    const deleteIcon = document.createElement("img");
-    deleteIcon.src = "./pics/delete.svg"; 
-    deleteIcon.alt = "Delete";
-    deleteIcon.className = "delete-icon";
+  //getting text from input
+  const taskText = taskInput.value;
 
-   //getting text from input
-    const taskText = taskInput.value;
-            
-    //checking if string is empty and if so adding an alert
-    if (taskText.length == "") {
-        alerting.innerHTML = "Input must not be empty";
-        alerting.classList.add("blinkingAlert");
-        return;
+  //checking if string is empty and if so adding an alert
+  if (taskText.length == "") {
+    alerting.innerHTML = "Input must not be empty";
+    alerting.classList.add("blinkingAlert");
+    return;
+  } else {
+    alerting.innerHTML = "";
+    alerting.classList.remove("blinkingAlert");
+  }
+  //creating a list
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  // adding listner to delete-icon and event to stop cklicklistner to bubble out to li
+  deleteIcon.addEventListener("click", function (event) {
+    li.remove();
+    event.stopPropagation();
+  });
+
+  // adding icons to li
+  // li.appendChild(checkIcon);
+  li.appendChild(deleteIcon);
+
+  // adding input to list
+  tasks.appendChild(li);
+
+  // clearing input
+  taskInput.value = "";
+
+  // delete all tasks by adding listner
+  // deleteAll.addEventListener("click", function() {
+  //     tasks.innerHTML = "";
+  // });
+
+  //Working to make task complete or not complete and counting tasks, now on li before on checkIcon
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
+    if (li.classList.contains("completed")) {
+      completedCount++;
     } else {
-        alerting.innerHTML = "";
-        alerting.classList.remove("blinkingAlert")
+      completedCount--;
     }
-        //creating a list
-        const li = document.createElement("li");
-        li.textContent = taskText;
-            
-        // adding listner to delete-icon and event to stop cklicklistner to bubble out to li
-        deleteIcon.addEventListener("click", function(event) {
-            li.remove();
-            event.stopPropagation();
-        });
 
+    document.querySelector("#completedCount").innerHTML =
+      completedCount + " completed";
+  });
 
-    
-        // adding icons to li
-        // li.appendChild(checkIcon);
-        li.appendChild(deleteIcon);
+  // add object to array
 
-        // adding input to list
-        tasks.appendChild(li);
-
-        // clearing input 
-        taskInput.value = "";
-        
-        
-        // delete all tasks by adding listner
-        // deleteAll.addEventListener("click", function() {
-        //     tasks.innerHTML = "";
-        // });
-        
-        //Working to make task complete or not complete and counting tasks, now on li before on checkIcon
-        li.addEventListener("click", function() {  
-            li.classList.toggle("completed");
-            if (li.classList.contains("completed")) {
-                completedCount++;
-            } else {
-                
-                completedCount--;
-            }
-
-           
-        
-        document.querySelector("#completedCount").innerHTML = completedCount + " completed";       
-         
-         });
-         
-
-       
-
-
-    // add object to array
-    
-    taskarray.push({ text: taskText, completed: false });
+  taskarray.push({ text: taskText, completed: false });
 });
